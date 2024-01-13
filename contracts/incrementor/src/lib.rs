71d802af4474d132767eef58aed2027c7baf7ef8
Arthur Ming <guobin.ming@pwc.com>
2024-01-13 22:24:58 +0800

    feat: add incrementor contract

---
+++ contracts/incrementor/src/lib.rs
@@ -0,0 +1,26 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec, log};
+
+const COUNTER: Symbol = symbol_short!("COUNTER");
+
+#[contract]
+pub struct IncrementorContract;
+
+#[contractimpl]
+impl IncrementorContract {
+   pub fn increment(env: Env) ->  u32 {
+        let mut count: u32 = env.storage().instance().get(&COUNTER).unwrap_or(0);
+
+        count += 1;
+        log!(&env, "count: {}", count);
+
+        env.storage().instance().set(&COUNTER, &count);
+
+        env.storage().instance().extend_ttl(100,100);
+
+        count
+    } 
+}
+
+#[cfg(test)]
+mod test;
